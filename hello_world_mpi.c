084f915290aab429ac1bf866eebfcc3e44004766
Bishwasls <bishwaslalsth@gmail.com>
2021-02-08 21:55:01 -0600

    name of rank and size changed

--- hello_world_mpi.c
+++ hello_world_mpi.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <mpi.h>
 
-int main(int argc, char** argv){
-  int process_Rank, size_Of_Cluster;
+int main(int argc, char** argv)
+{
+  int my_rank, nproc;
   
   MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &size_Of_Cluster);
-  MPI_Comm_rank(MPI_COMM_WORLD, &process_Rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   
-  printf("Hello World from process %d of %d\n", process_Rank, size_Of_Cluster);
-
+  printf("Hello World from process %d of %d\n", my_rank, nproc);
+  
   MPI_Finalize();
   return 0;
 }
